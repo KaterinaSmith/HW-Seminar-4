a36fed4af08bed8e43a63955fc5f537ab398f940
Katerina <katerina3113@bk.ru>
2023-01-31 11:40:02 +0300

    Домашнее задание 2

---
+++ hm2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int number = ReadInt ("Введите число:");
+int length = NumberLength(number);
+SumNumbers (number, length);
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int NumberLength(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index = index + 1;
+    }
+    return index;
+}
+void SumNumbers (int n, int length)
+{
+    int sum = 0;
+    for (int i = 1; i <= length; i++)
+    {
+        sum += n % 10;
+        n /=10;
+    }
+    Console.WriteLine(sum);
+}
