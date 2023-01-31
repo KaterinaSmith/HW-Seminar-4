0fbcfd689dc0744e0a8b3b526690f7d71b60620a
Katerina <katerina3113@bk.ru>
2023-01-31 14:08:05 +0300

    Домашнее задание 3

---
+++ hm3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int [] CreateArray()
+{
+    int [] Array = new int[8];
+    Random number =  new Random();
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Array[i] = number.Next(0,1000);
+    }
+    return Array;
+}
+int [] a = CreateArray();
+
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+    Console.Write($" {array[i]} ");
+    }
+}
+PrintArray(a);
